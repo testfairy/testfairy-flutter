ae6264a19d9305ae56d21f42f4a7df6c6d9d016a
Diego Perini <diego@dperini.com>
2020-11-12 18:48:56 +0300

    Fixed lint errors for example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:http/http.dart' as http;
 import 'package:testfairy/testfairy.dart';
 
