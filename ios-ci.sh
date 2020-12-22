9f947e0af8eb6665f6d78395b5187f408547aaeb
Diego Perini <diego@dperini.com>
2020-12-22 18:29:58 +0300

    Testing iOS CI

--- ios-ci.sh
+++ ios-ci.sh
@@ -6,8 +6,8 @@ gem install -n /usr/local/bin cocoapods -v 1.7.5
 alias pod="pod _1.7.5_"
 pod --version
 pip install six
-brew update
-brew install automake autoconf libtool wget libimobiledevice ideviceinstaller ios-deploy
+brew update || true
+brew install libimobiledevice ideviceinstaller ios-deploy
 cd ..
 git clone https://github.com/flutter/flutter.git -b dev --depth 1
 cd testfairy-flutter
