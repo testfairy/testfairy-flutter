9ef580165c288468ca4a64a070b28d41479b5838
Diego Perini <diego.perini@saucelabs.com>
2021-03-18 23:24:40 +0300

    Fixed reference in dart 1 tests

--- example-dart1/test/widget_test.dart
+++ example-dart1/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:testfairy_flutter_example/main.dart';
+import 'package:testfairy_flutter_example_dart1/main.dart';
 
 void main() {
   testWidgets('Verify Platform version', (WidgetTester tester) async {
