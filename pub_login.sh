2143a43bdfa3c2f8b74da86189514b06a2eb43b3
Diego Perini <diego@dperini.com>
2021-12-16 13:44:13 +0300

    Fixed flutter pub login

--- pub_login.sh
+++ pub_login.sh
@@ -23,9 +23,20 @@ if [ -z "${PUB_DEV_PUBLISH_EXPIRATION}" ]; then
   exit 1
 fi
 
-# Create credentials.json file.
+# Create credentials.json files
+
 mkdir -p ~/.pub-cache/
 cat <<EOF > ~/.pub-cache/credentials.json
+{
+  "accessToken":"${PUB_DEV_PUBLISH_ACCESS_TOKEN}",
+  "refreshToken":"${PUB_DEV_PUBLISH_REFRESH_TOKEN}",
+  "tokenEndpoint":"${PUB_DEV_PUBLISH_TOKEN_ENDPOINT}",
+  "scopes":["https://www.googleapis.com/auth/userinfo.email","openid"],
+  "expiration":${PUB_DEV_PUBLISH_EXPIRATION}
+}
+
+mkdir -p ~/flutter/.pub-cache/
+cat <<EOF > ~/flutter/.pub-cache/credentials.json
 {
   "accessToken":"${PUB_DEV_PUBLISH_ACCESS_TOKEN}",
   "refreshToken":"${PUB_DEV_PUBLISH_REFRESH_TOKEN}",
